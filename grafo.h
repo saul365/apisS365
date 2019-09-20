65f9b431d7c518ef15824e224d6902a65b67ab97
saul365 <saulbh365@comunidad.unam.mx>
2019-09-20 12:41:06 -0500

    grafo added

---
+++ grafo.h
@@ -0,0 +1,18 @@
+#ifndfef _GRAFO_INC
+#define _GRAFO_INC_
+
+class nodo{
+public:
+	int id;
+	int* calles;
+	int max;
+	int conexiones;
+	int* ids;
+	nodo(int id, int max);
+}
+
+class grafo{
+public:
+	
+}
+#endif
