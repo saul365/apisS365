2daac49d39720e22716d3841f7c5d2c8710f2407
saul365 <saul_bh365@comunidad.unam.mx>
2019-09-21 14:18:51 -0500

    changed for

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
 #include "Grafo.h"
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main(){
-	int n=0;
-	cout<<"Ingrese cuantas casas hay: "<<endl;
+	int n;
+	cout<<"Ingrese cuantas casas hay ";
+	//scanf("%d",&n);//ultima opción
 	cin>>n;
 	cout<<n;
 	int prim=0,seg=0;
 	Grafo casas=Grafo(n);
-	for(int i;i<n;i++){
+	for(int i=0;i<n-1;i++){
 		cout<<"Ingrese una calle:";
+		//scanf("%d %d",&prim,&seg);//si no te deja esta es nuestra ultima opción
 		cin>>prim>>seg;
 		casas.nodoAdd(prim,seg);
 		casas.nodoAdd(seg,prim);
