d79d6cd34bec7f7cf340560d3144d3bcd387aabe
saul365 <saulbh365@comunidad.unam.mx>
2019-09-21 11:07:23 -0500

    casi listo

---
+++ Grafo.h
@@ -0,0 +1,18 @@
+#ifndef _GRAFO_INC_
+#define _GRAFO_INC_
+#include <stdlib.h>
+#include <vector>
+
+class Grafo{
+public:
+	Grafo(int num);
+	void nodoAdd(int id, int value);
+	void busqueda(void);
+	bool reco(int i,int search,std::vector<std::vector<int>> nodos);
+	std::vector<std::vector<int>> nodos;
+	std::vector<std::vector<int>> calles;
+	std::vector<int> peligro;
+	std::vector<bool> visited;
+	int num;
+};
+#endif
--- grafo.cpp
+++ grafo.cpp
@@ -1,38 +1,49 @@
-#include "grafo.h"
-
-/*
-nodo::nodo(int id,int max){
-	this.id=id;	
-	this.max=max;	
-	calles.reserve(max);
-	ids.reserve(max);
-}
-*/
+#include "Grafo.h"
 
 
-grafo::grafo(int num){
+Grafo::Grafo(int num){
+	this->num=num;
 	nodos.reserve(num);
-	for(i=0;i<num;i++){
+	for(int i=0;i<num;i++){
 		nodos.push_back(std::vector<int>(num-1));
-		peligro.pushback(0);
+		peligro.push_back(0);
+		visited.push_back(false);
 	}
 }
-grafo::search(){
-	for(int i=0;i<num-2;i++){
+Grafo::busqueda(void){
+		for(int i=0;i<num-2;i++){
 		for(int search=i+1;search<num+1;search++){
 			bool done=false;
-			do{
 				for(int j=0;j<nodos[1].size();j++){
 					if(nodos[i][j]==search){
 						done=true;
 						
 					}
-				}
 				if(!done){
+				for(int j=0;j<nodos[1].size();j++){
+						if(!visited[nodos[i][j]]){
+							if(reco(nodos[i][j],search,nodos)){
+								peligro[calles[i][j]]++;
+								
+							}
+						}
 					
 				}
-			}while(!done);	
-		}
+		}		
 	}
 }
 
+Grafo::reco(int i,int search, vector nodos){
+				for(int j=0;j<nodos[1].size();j++){
+						if(!visited[nodos[i][j]]){
+							if(reco(nodos[i][j],search,nodos)){
+								peligro[calles[i][j]]++;
+								return true;
+							}
+						}
+	}
+	return false;
+}
+Grafo::nodoAdd(int id,int value){
+	calles[id].push_back(value);
+}
